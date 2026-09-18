d73bfac4a2b9a2a9e7ce20c2ed10531ed3c6a569
Mohammad Asim <asim.lpu07@gmail.com>
2026-09-18 23:06:03 +0530

    Time: 5 ms (51.16%), Space: 45 MB (36.46%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String simplifyPath(String path) {
+        
+
+        Stack<String> stack = new Stack<>();
+
+        String[] arr = path.split("/");
+
+        for (String s : arr) {
+
+            if (s.equals("") || s.equals(".")) {
+                continue;
+            }
+
+            if (s.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(s);
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        for (String s : stack) {
+            ans.append("/").append(s);
+        }
+
+        return ans.length() == 0 ? "/" : ans.toString();
+    }
+}
